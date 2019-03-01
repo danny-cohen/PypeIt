59661672cc911a33010b3f96e332c66bf23baa71
joe <joe@physics.ucsb.edu>
2019-02-28 16:24:43 -0800

    added dependency to IPython

--- pypeit/scripts/coadd_1dspec.py
+++ pypeit/scripts/coadd_1dspec.py
@@ -3,12 +3,6 @@
 """
 Wrapper to the linetools XSpecGUI
 """
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import argparse
 
 def parser(options=None):
