d6e14f75b7483755f51bc593af0a1671aa4e6f55
Feige Wang <fgwang.astro@gmail.com>
2019-06-23 12:48:36 -0700

    fixing lbt bpm

--- pypeit/spectrographs/lbt_mods.py
+++ pypeit/spectrographs/lbt_mods.py
@@ -227,15 +227,15 @@ class LBTMODS1RSpectrograph(LBTMODSSpectrograph):
         hdu.close()
 
         # Apply the mask
-        self.bpm_img[6278//xbin:6288//xbin, 1545//ybin:1634//ybin] = 1
-        self.bpm_img[4202//xbin:4203//xbin, 1475//ybin:1544//ybin] = 1
-        self.bpm_img[3551//xbin:3557//xbin, 2392//ybin:2903//ybin] = 1
-        self.bpm_img[3553//xbin:3557//xbin, 1455//ybin:1544//ybin] = 1
+        self.bpm_img[6278//xbin:6289//xbin, 1544//ybin:1634//ybin] = 1
+        self.bpm_img[4202//xbin:4204//xbin, 1474//ybin:1544//ybin] = 1
+        self.bpm_img[3551//xbin:3558//xbin, 2391//ybin:2903//ybin] = 1
+        self.bpm_img[3553//xbin:3558//xbin, 1454//ybin:1544//ybin] = 1
 
-        self.bpm_img[5650//xbin, 1281//ybin:1544//ybin] = 1
-        self.bpm_img[4780//xbin, 1407//ybin:1536//ybin] = 1
-        self.bpm_img[3554//xbin, 1545//ybin:2392//ybin] = 1
-        self.bpm_img[163//xbin, 1545//ybin:1963//ybin] = 1
+        self.bpm_img[5650//xbin, 1280//ybin:1544//ybin] = 1
+        self.bpm_img[4780//xbin, 1406//ybin:1536//ybin] = 1
+        self.bpm_img[3554//xbin, 1544//ybin:2392//ybin] = 1
+        self.bpm_img[163//xbin, 1544//ybin:1963//ybin] = 1
 
         return self.bpm_img
 
@@ -348,11 +348,11 @@ class LBTMODS1BSpectrograph(LBTMODSSpectrograph):
         hdu.close()
 
         # Apply the mask
-        self.bpm_img[6390//xbin:6391//xbin, 1263//ybin:1545//ybin] = 1
+        self.bpm_img[6390//xbin:6392//xbin, 1262//ybin:1545//ybin] = 1
 
-        self.bpm_img[3064//xbin, 1438//ybin:1937//ybin] = 1
-        self.bpm_img[6490//xbin, 1162//ybin:1545//ybin] = 1
-        self.bpm_img[7306//xbin, 784//ybin:1531//ybin] = 1
+        self.bpm_img[3064//xbin, 1437//ybin:1937//ybin] = 1
+        self.bpm_img[6490//xbin, 1161//ybin:1545//ybin] = 1
+        self.bpm_img[7306//xbin, 783//ybin:1531//ybin] = 1
 
         return self.bpm_img
 
@@ -470,16 +470,16 @@ class LBTMODS2RSpectrograph(LBTMODSSpectrograph):
         hdu.close()
 
         # Apply the mask
-        self.bpm_img[6148//xbin:6149//xbin, 1334//ybin:1544//ybin] = 1
-        self.bpm_img[6207//xbin:6208//xbin, 1397//ybin:1544//ybin] = 1
+        self.bpm_img[6148//xbin:6150//xbin, 1333//ybin:1544//ybin] = 1
+        self.bpm_img[6207//xbin:6209//xbin, 1396//ybin:1544//ybin] = 1
 
-        self.bpm_img[6101//xbin, 1343//ybin:1544//ybin] = 1
-        self.bpm_img[6159//xbin, 1400//ybin:1544//ybin] = 1
-        self.bpm_img[6189//xbin, 1317//ybin:1544//ybin] = 1
-        self.bpm_img[7552//xbin, 1545//ybin:2771//ybin] = 1
-        self.bpm_img[7504//xbin, 1545//ybin:2774//ybin] = 1
-        self.bpm_img[4203//xbin, 1//ybin:1544//ybin] = 1
-        self.bpm_img[4155//xbin, 1//ybin:1544//ybin] = 1
+        self.bpm_img[6101//xbin, 1342//ybin:1544//ybin] = 1
+        self.bpm_img[6159//xbin, 1399//ybin:1544//ybin] = 1
+        self.bpm_img[6189//xbin, 1316//ybin:1544//ybin] = 1
+        self.bpm_img[7552//xbin, 1544//ybin:2771//ybin] = 1
+        self.bpm_img[7504//xbin, 1544//ybin:2774//ybin] = 1
+        self.bpm_img[4203//xbin, 0:1544//ybin] = 1
+        self.bpm_img[4155//xbin, 0:1544//ybin] = 1
 
         return self.bpm_img
 
@@ -594,22 +594,22 @@ class LBTMODS2BSpectrograph(LBTMODSSpectrograph):
         hdu.close()
 
         # Apply the mask
-        self.bpm_img[5176//xbin:5178//xbin, 550//ybin:1544//ybin] = 1
-        self.bpm_img[5176//xbin:5178//xbin, 1545//ybin:1628//ybin] = 1
-        self.bpm_img[4408//xbin:4409//xbin, 1545//ybin:2661//ybin] = 1
-        self.bpm_img[4408//xbin:4410//xbin, 2661//ybin:2663//ybin] = 1
-        self.bpm_img[2495//xbin:2498//xbin, 1327//ybin:1544//ybin] = 1
-        self.bpm_img[2391//xbin:2393//xbin, 1049//ybin:1051//ybin] = 1
-        self.bpm_img[1974//xbin:1979//xbin, 807//ybin:1544//ybin] = 1
-        self.bpm_img[1975//xbin:1979//xbin, 1545//ybin:1607//ybin] = 1
-        self.bpm_img[1972//xbin:1973//xbin, 1588//ybin:1589//ybin] = 1
-        self.bpm_img[274//xbin:277//xbin, 1342//ybin:1544//ybin] = 1
-        self.bpm_img[275//xbin:277//xbin, 1252//ybin:1341//ybin] = 1
-        self.bpm_img[276//xbin:277//xbin, 1243//ybin:1251//ybin] = 1
-        self.bpm_img[274//xbin:276//xbin, 1545//ybin:3066//ybin] = 1
+        self.bpm_img[5176//xbin:5179//xbin, 549//ybin:1544//ybin] = 1
+        self.bpm_img[5176//xbin:5179//xbin, 1544//ybin:1628//ybin] = 1
+        self.bpm_img[4408//xbin:4410//xbin, 1544//ybin:2661//ybin] = 1
+        self.bpm_img[4408//xbin:4411//xbin, 2660//ybin:2663//ybin] = 1
+        self.bpm_img[2495//xbin:2499//xbin, 1326//ybin:1544//ybin] = 1
+        self.bpm_img[2391//xbin:2394//xbin, 1048//ybin:1051//ybin] = 1
+        self.bpm_img[1974//xbin:1980//xbin, 806//ybin:1544//ybin] = 1
+        self.bpm_img[1975//xbin:1980//xbin, 1544//ybin:1607//ybin] = 1
+        self.bpm_img[1972//xbin:1974//xbin, 1587//ybin:1589//ybin] = 1
+        self.bpm_img[274//xbin:278//xbin, 1341//ybin:1544//ybin] = 1
+        self.bpm_img[275//xbin:278//xbin, 1251//ybin:1341//ybin] = 1
+        self.bpm_img[276//xbin:278//xbin, 1242//ybin:1251//ybin] = 1
+        self.bpm_img[274//xbin:277//xbin, 1544//ybin:3066//ybin] = 1
 
-        self.bpm_img[2392//xbin, 1052//ybin:1544//ybin] = 1
-        self.bpm_img[275//xbin, 1221//ybin:1242//ybin] = 1
+        self.bpm_img[2392//xbin, 1051//ybin:1544//ybin] = 1
+        self.bpm_img[275//xbin, 1220//ybin:1242//ybin] = 1
 
         return self.bpm_img
 
